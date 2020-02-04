ff500fd0faf0934ca7ea0ba778b973904cd93764
ups7write <anatoli.mihov@gmail.com>
2020-02-04 14:06:19 +0200

    form validation and style form

--- src/js/script.js
+++ src/js/script.js
@@ -21,44 +21,77 @@ document.querySelector('.next').addEventListener('click', function () {
 });
 // Code for tiny-slider END
 $(document).ready(function () {
-	$(function () {
-		// Code for catalog tabs START
-		$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-			$(this)
-				.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-				.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-		});
+	// Code for catalog tabs START
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 
 
-		// script for change links classes
-		function toggleSlide(item) {
-			$(item).each(function (i) {
-				$(this).on('click', function (e) {
-					e.preventDefault();
-					$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-					$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-				});
+	// script for change links classes
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 			});
-		}
-
-		toggleSlide('.catalog-item__link');
-		toggleSlide('.catalog-item__back');
-		// Code for catalog tabs END
-
-		// Modal windows script
-		$('[data-modal=consultation').on('click', function () {
-			$('.overlay, #consultation').fadeIn('100');
 		});
-		// script to close items
-		$('.modal__close').on('click', function () {
-			$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-		});
+	}
 
-		$('.button_buy').each(function (i) {
-			$(this).on('click', function () {
-				$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-				$('.overlay, #order').fadeIn('100');
-			});
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+	// Code for catalog tabs END
+
+	// Modal windows script
+	$('[data-modal=consultation').on('click', function () {
+		$('.overlay, #consultation').fadeIn('100');
+	});
+
+	// script to close modal windows
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+		document.location.reload();// reload page
+	});
+
+	// получаем и вставляем текст в элемент с классом '.className'
+	$('.button_buy').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('100');
 		});
 	});
+
+	// validation form code
+	function validatorForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 3,
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true,
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста введите ваше имя.",
+					minlength: jQuery.validator.format("Минимальное кол-во символов: {0}"),
+				},
+				phone: "Пожалуйста введите номер телефона",
+				email: {
+					required: "Введите ваш email address",
+					email: "Ваш адрес должен быть в формате: name@domain.com",
+				}
+			}
+		});
+	}
+
+	validatorForms('#consultation-form');
+	validatorForms('#consultation .feed-form');
+	validatorForms('#order .feed-form');
 });
